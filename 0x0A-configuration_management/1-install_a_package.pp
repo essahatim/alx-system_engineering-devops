9b3817fcecd584101bc32d28b68e385d6a5e701b
essahatim <hatimessabagh@gmail.com>
2024-03-22 19:46:41 -0700

    update file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Install Flask from pip3
-package {'flask':
-  ensure   => '2.1.0',
+# Puppet Manifest: Install Flask version 2.1.0
+
+package { 'flask':
+  ensure => '2.1.0',
   provider => 'pip3',
 }
