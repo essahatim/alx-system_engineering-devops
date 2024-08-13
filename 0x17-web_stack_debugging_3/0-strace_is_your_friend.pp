e7ad224a98ccab8770d0b01a0d9d6cd74112c487
essahatim <135241099+essahatim@users.noreply.github.com>
2024-08-13 23:21:02 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,7 +0,0 @@
-# Fix 500 error when a GET HTTP method is requested to Apache web server
-
-exec { 'fix-issue':
-  command     => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path        => '/bin:/usr/bin:/sbin:/usr/sbin',
-  refreshonly => true,
-}
