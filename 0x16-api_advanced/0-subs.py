da08e8afc1a66ccfc37028ff24185ed089686714
essahatim <hatimessabagh@gmail.com>
2024-08-14 06:31:12 -0700

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,11 +13,11 @@ def number_of_subscribers(subreddit):
         The number of subscribers to the subreddit
         0 in error
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "OkSatisfaction9309"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        return (response.json().get("data").get("subscribers"))
+        return (response.json().get("data", {}).get("subscribers", 0))
     else:
         return (0)
